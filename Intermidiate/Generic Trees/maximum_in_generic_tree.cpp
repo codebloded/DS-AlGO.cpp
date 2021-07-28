5bc0eac906884523f148d177b4637c89499a557f
Rohan <rohanrajput16.phy@gmail.com>
2021-07-28 14:44:45 +0530

    maximum node

---
+++ Intermidiate/Generic Trees/maximum_in_generic_tree.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    vector<Node *> children;
+};
+
+Node *construct(vector<int> arr)
+{
+    stack<Node *> st;
+    Node *root = NULL;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        Node *node = new Node;
+        node->data = arr[i];
+        if (arr[i] == -1)
+        {
+            st.pop();
+        }
+        else
+        {
+            if (st.size() > 0)
+            {
+                Node *newNode = st.top();
+                newNode->children.push_back(node);
+            }
+            else
+            {
+                root = node;
+            }
+            st.push(node);
+        }
+    }
+    return root;
+}
+
+int maximum(Node *node)
+{
+    int maxE = INT_MIN;
+    for (Node *child : node->children)
+    {
+        int cm = maximum(child);
+        maxE = max(cm, maxE);
+    }
+    maxE = max(node->data, maxE);
+    return maxE;
+}
+
+int main()
+{
+    int size;
+    cin >> size;
+    vector<int> arr(size);
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+
+    Node *root = construct(arr);
+    cout << maximum(root);
+    return 0;
+}
