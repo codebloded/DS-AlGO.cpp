06ad3c5394016681642aa358db2df46caeff8494
Rohan <rohanrajput16.phy@gmail.com>
2021-07-29 23:19:51 +0530

    zigzag traversal in generic tree.

---
+++ Intermidiate/Generic Trees/zig_zag.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#include "tree.h"
+using namespace std;
+
+void zigzag(Node *node)
+{
+    stack<Node *> ms; //main stack
+    ms.push(node);
+    int level = 1;
+
+    stack<Node *> cs; // child stack
+    while (ms.size() > 0)
+    {
+        Node *temp = ms.top();
+        ms.pop();
+        if (temp)
+        {
+            cout << temp->data << " ";
+
+            if (level % 2 == 1)
+            {
+                for (int i = 0; i < temp->children.size(); i++)
+                {
+                    Node *child = temp->children.at(i);
+                    cs.push(child);
+                }
+            }
+            else
+            {
+                for (int i = temp->children.size() - 1; i >= 0; i--)
+                {
+                    Node *child = temp->children.at(i);
+                    cs.push(child);
+                }
+            }
+        }
+        if (ms.size() == 0)
+        {
+            swap(ms, cs);
+            level++;
+            cout << endl;
+        }
+    }
+}
+
+int main()
+{
+    int size;
+    cin >> size;
+    vector<int> arr(size);
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+
+    Node *root = construct(arr);
+    zigzag(root);
+    return 0;
+}
